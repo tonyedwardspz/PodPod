af6f917c1fa291486c94ab4098a11fbb69207e1a
Tony Edwards <tony.edwards@gmail.com>
2024-08-17 19:35:36 +0100

    Adds library page

--- PodPod/Models/Podcast.cs
+++ PodPod/Models/Podcast.cs
@@ -4,6 +4,11 @@ public class Podcast
 {
 	public string Title { get; set; }
 	public string FeedUrl { get; set; }
+	private string? cover;
+	public string? Cover { 
+		get => cover ?? "cover.png";
+		set => cover = value; 
+	}
 
 	public Podcast(string title, string feedUrl)
 	{
